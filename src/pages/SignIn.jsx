601d3706c6684e3146f25d33accb7268b9e36cb4
Austine Jack Were <austinejackwere@gmail.com>
2023-04-23 20:29:49 +0300

    sign in function added

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
-import{AiFillEyeInvisible, AiFillEye} from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 export default function SignIn() {
-    const [showPassword, setShowPassword] = useState(false);
+    const [showPassword] = useState(false);
     const [formData, setFormData] = useState({
         email:"",
         password:"",
@@ -37,7 +37,7 @@ export default function SignIn() {
                     text-gray-700 bg-white border-gray-300
                     rounded transition ease-in-out'
                     />
-                    <div className='relative mb-6'>
+                    <div>
                     <input 
                     type={showPassword ? "text" : "password"}
                     id="password"
@@ -48,12 +48,26 @@ export default function SignIn() {
                     text-gray-700 bg-white border-gray-300
                     rounded transition ease-in-out'
                     />
-                    {showPassword ? (
-                        <AiFillEyeInvisible className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=>setShowPassword((prevState)=>!prevState)}/>) :
-                        (<AiFillEye className='absolute right-3 top-3 text-xl cursor-pointer' />)
-                    }
+                    </div>
+                    <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+                        <p className='mb-6'>
+                            Don't have an account?
+                            <Link to={"/sign-up"}
+                                className="text-red-500 hover:text-red-800
+                                transition duration-200 ease-in-out ml-1">Register </Link>
+                        </p>
+                        <p>
+                            <Link to={"forgot-password"} className="text-blue-600 hover:text-blue-900
+                                transition duration-200 ease-in-out ml-1"> Forgot password?</Link>
+                        </p>
                     </div>
                 </form>
+                <button className='w-ful bg-blue-500 text-white px-7 py-3
+                text-sm font-medium uppercase rounded 
+                shadow-md hover:bg-blue-700 
+                transition duration-150 ease-in-out 
+                hover:shadow-lg active:bg-blue-800'
+                type="submit">Sign In</button>
             </div>
         </div>
     </section>
