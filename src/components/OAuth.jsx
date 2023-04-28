de6ac0f37e9d60b9fe5a761e356d9015fc449c35
Austine Jack Were <austinejackwere@gmail.com>
2023-04-28 17:21:18 +0300

    OAuth functionality updated

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,37 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import React from 'react';
 import {FcGoogle} from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from '../firebase';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 
 export default function OAuth() {
+  async function onGoogleClick(){
+    try {
+      const auth = getAuth()
+      const provider = new
+      GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      // check for the user
+      const docRef = doc(db, "users", users.uid)
+      const docSnap = await getDoc(docRef)
+
+      if(!docSnap.exists()){
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp,
+        })
+      }
+    } catch (error){
+      toast.error("failed to authorize with Google")
+    }
+  }
   return (
-    <button className="flex items-center justify-center w-full bg-red-600 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150
+    <button type="button" 
+    onClick={onGoogleClick}
+    className="flex items-center justify-center w-full bg-red-600 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150
     ease-in-out rounded">
         <FcGoogle className='text-2xl bg-white
         rounded-full mr-2'/>
