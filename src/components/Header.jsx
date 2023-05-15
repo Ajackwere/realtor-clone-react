c60c368a1f0e8b558c2d611f87f7a5ab2e2a7420
Austine Jack Were <austinejackwere@gmail.com>
2023-05-15 20:54:33 +0300

    bug fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import {useLocation, useNavigate} from "react-router-dom";
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 export default function Header() {
-    const [pageState, setPageState] = useState("Sign in")
+    const [pageState, setPageState] = useState("Sign in");
     const location = useLocation();
     const navigate = useNavigate();
     const auth = getAuth();
