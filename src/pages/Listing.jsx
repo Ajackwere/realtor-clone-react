a109173d8b2e186284fe4632ace146eb9a7d0e01
Ajackwere <austinejackwere@gmail.com>
2023-07-11 20:45:21 +0300

    Styling for the listing added

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,7 +11,7 @@ import SwiperCore, {
   Navigation,
   Pagination,
 } from "swiper";
-import {FaShareAlt} from "react-icons/fa";
+import { FaShareAlt } from "react-icons/fa";
 import "swiper/css/bundle";
 
 export default function Listing() {
@@ -50,24 +50,38 @@ export default function Listing() {
               className="relative w-full overflow-hidden h-[300px]"
               style={{
                 background: `url(${listing.imgUrls[index]}) center no-repeat`,
-                backgroundSize: "cover"
+                backgroundSize: "cover",
               }}
             ></div>
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer
       border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
-      onClick={()=>{
-        navigator.clipboard.writeText(window.location.href)
-        setShareLinkCopied(true);
-        setTimeout(()=>{setShareLinkCopied(false);
-        }, 2000);
-      }}>
-        <FaShareAlt className="text-lg text-slate-500"/>
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShareAlt className="text-lg text-slate-500" />
       </div>
-      {shareLinkCopied && <p className="fixed top-[23%] right-[5%] font-semibold border-2 
-      border-gray-400 rounded-md bg-white z-10 p-2">Link Copied</p>}
+      {shareLinkCopied && (
+        <p
+          className="fixed top-[23%] right-[5%] font-semibold border-2 
+      border-gray-400 rounded-md bg-white z-10 p-2"
+        >
+          Link Copied
+        </p>
+      )}
+      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto
+      p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
+        <div className="bg-pink-300 w-full h-[200px] lg-[400px]"></div>
+        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+      </div>
     </main>
   );
 }
